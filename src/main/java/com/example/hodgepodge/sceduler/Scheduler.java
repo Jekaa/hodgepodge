48203ef86c6a5b91c511ae51249cdac5bc7b4c92
a16731068 <Gordienko.E.Evg@sberbank.ru>
2020-08-01 11:06:27 +0300

    fix scheduler

--- src/main/java/com/example/hodgepodge/sceduler/Scheduler.java
+++ src/main/java/com/example/hodgepodge/sceduler/Scheduler.java
@@ -1,17 +1,20 @@
 package com.example.hodgepodge.sceduler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-@Configuration
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
 @Slf4j
 public class Scheduler {
 
-    @Bean
-    @Scheduled(fixedRate = 3000)
-    public void scheduledLogger() {
-        log.info("Scheduler working");
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
     }
 }
